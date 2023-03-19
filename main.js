719dcfc14f7c4e833f8056cc2f75db557ac24345
MahmudovMuhammadrizo24 <mahmudovmuhammadrizo877@gmail.com>
2023-03-19 16:33:27 +0500

    change  javaScript code

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-const main = document.querySelector(".div");
+const buttons = document.querySelectorAll("button");
+const password_length = document.querySelector(".password_length");
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        password_length.innerText = button.innerText
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
